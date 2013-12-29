1a540f3bc3351642ba7c6561ff3506c35916c205
Blacklight Shining <blacklightshining@derpymail.org>
2013-12-29 11:45:58 -0800

    Various changes to handler.py
    It only imports re.compile, rather than the entire re module.
    
    Handler:
    Added the missing comma in __str__()'s format string.
    
    HandlerManager:
    add_handlers() now sorts the Handler lists that were added to, eliminating the need for a sort_handlers() . Sorting was also unreversed.
    remove_handlers() now takes actual Handlers to remove, rather than IDs. It also removes empty Handler lists.
    sort_handlers() and print_handlers() were removed.
    
    All of it now fits in 79 columns. :)

--- handler.py
+++ handler.py
@@ -1,44 +1,51 @@
-import re
+from re import compile as re_compile
 from operator import attrgetter
 
-class Handler():
+class Handler:
     def __init__(self, event, priority, pattern, callback):
         self.event = event
         self.priority = priority
-        self.pattern = re.compile(pattern)
+        # If you pass re.compile() a compile()d regex object, it returns it
+        # (presumably without further processing).
+        # Thus, /pattern/ may be either a string or a regex object.
+        self.pattern = re_compile(pattern)
         self.callback = callback
 
     def __str__(self):
-        return "Handler(event=%s, priority=%d, pattern=%s callback=%s)" % (repr(self.event), self.priority, repr(self.pattern.pattern), self.callback)
+        return "Handler(event=%s, priority=%d, pattern=%s, callback=%s)" % (
+            repr(self.event), self.priority,
+            repr(self.pattern.pattern), self.callback,
+            )
 
     def __repr__(self):
         return self.__str__()
 
-class HandlerManager():
+class HandlerManager:
     def __init__(self):
         self.handler_lists = {}
 
     def add_handlers(self, *handlers):
+        changed_events = set()
         for handler in handlers:
+            changed_events.add(handler.event)
             try:
                 self.handler_lists[handler.event].append(handler)
             except KeyError:
                 self.handler_lists[handler.event] = [handler]
+        for event in changed_events:
+            self.handler_lists[event].sort(key=attrgetter("priority"))
 
-    def remove_handlers(self, *handler_ids):
-        for handler_list in self.handler_lists.values():
-            # TODO: make this loop Pythonic:
-            for i in range(len(handler_list) - 1):
-                if id(handler_list[i]) in handler_ids:
-                    del handler_list[i]
-
-    def sort_handlers(self):
-        for event in self.handler_lists.keys():
-            self.handler_lists[event].sort(key=attrgetter("priority"), reverse=True)
-
-    def print_handlers(self):
-        for event in self.handler_lists.keys():
-            handler_list = self.handler_lists[event]
-            print("%s: %s" % (event, repr(handler_list)))
+    def remove_handlers(self, *handlers):
+        changed_events = set()
+        for handler in handlers:
+            changed_events.add(handler.event)
+            try:
+                self.handler_lists[handler.event].remove(handler)
+            except ValueError:
+                # The Handler wasn't in the list. That's perfectly fine.
+                pass
+        for event in changed_events:
+            if not self.handler_lists[event]:
+                del self.handler_lists[event]
 
 # TODO: Enum for event priorities or something?
