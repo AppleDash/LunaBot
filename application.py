0653289b20ccae368670b9ed87a5d03d99f9e3ba
Blacklight Shining <blacklightshining@derpymail.org>
2013-12-30 16:28:55 -0800

    Changed application.py to use a config dir instead of a single config file

--- application.py
+++ application.py
@@ -2,34 +2,42 @@ from argparse import ArgumentParser
 import errno
 from json import load as json_load, dump as json_dump
 from os import makedirs
-from os.path import dirname, isdir, normpath
+import os.path
 
 from lunabot.default_config import default_config
+from lunabot.handler import HandlerManager
 
 config = None
+handlers = None
 
 def main():
     arg_parser = ArgumentParser(prog="lunabot")
-    arg_parser.add_argument("-c", "--config", dest="config_file_name")
+    arg_parser.add_argument("-d", "--config-dir", "--configuration-directory",
+                            dest="config_dir")
     args = arg_parser.parse_args()
 
-    # According to the docs, makedirs() gets confused if
-    # the path you give it has `..` in it. So, just in case...
-    config_file_name = normpath(args.config_file_name)
+    # According to the docs, makedirs() gets confused if the path you give it
+    # has `..` in it. So normpath() it just in case.
+    config_dir = os.path.abspath(os.path.normpath(args.config_dir))
+    config_file_name = os.path.join(config_dir, 'lunabot.json')
 
     global config
     try:
         with open(config_file_name, "r") as config_file:
             config = json_load(config_file)
     except FileNotFoundError:
-        config_file_parent = dirname(config_file_name)
+        config = default_config.copy()
         try:
-            makedirs(config_file_parent, exist_ok=True)
-        except OSError as exception:
-            if exception.errno == errno.EEXIST and isdir(config_file_parent):
-                pass
-            else:
-                raise
-        config = dict(default_config)
-        with open(config_file_name, "w") as config_file:
-            json_dump(config, config_file)
+            with open(config_file_name, "x") as config_file:
+                json_dump(config, config_file)
+        except FileNotFoundError:
+            try:
+                makedirs(config_dir, exist_ok=True)
+            except OSError as exception:
+                if exception.errno == errno.EEXIST and os.path.isdir(config_dir):
+                    pass
+                else:
+                    raise
+
+    global handlers
+    handlers = HandlerManager()
